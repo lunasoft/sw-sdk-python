964a845e5bb9499d54de652a80d32eb3f4a2f6c9
Emmanuel <emmanuel.contreras@sw.com.mx>
2018-06-05 17:58:49 -0500

    Se agregan pruebas unitarias

--- SWSDKPython.py
+++ SWSDKPython.py
@@ -1,29 +1,33 @@
 import requests
 
 
-
 class SWSDKPython:
 
-    def authenticate(self, url, user, password):
+    @staticmethod
+    def authenticate(url, user, password):
         headers = {'user': user, 'password': password, 'Cache-Control': "no-cache"}
         response = requests.request("POST", url + "/security/authenticate", headers=headers)
         return response.text
 
-    def stamp(self, url, token, xml, version, b64=False):
+    @staticmethod
+    def stamp(url, token, xml, version, b64=False):
         if b64 is True:
             bs64 = "/b64"
         else:
             bs64 = ""
         boundary = "----=_Part_11_11939969.1490230712432"
         payload = "--" + boundary + "\r\nContent-Type: text/xml\r\nContent-Transfer-Encoding: binary\r\nContent-Disposition: " \
-                  "form-data; name=\"xml\"; filename=\"xml\"\r\n\r\n" + str(xml) + "\r\n--" + boundary + "-- "
+                                    "form-data; name=\"xml\"; filename=\"xml\"\r\n\r\n" + str(
+            xml) + "\r\n--" + boundary + "-- "
         headers = {
             'Authorization': "bearer " + token,
             'Content-Type': "multipart/form-data; boundary=\"" + boundary + "\""
         }
-        response = requests.request("POST", url + "/cfdi33/stamp/" + version + "/" + bs64, data=payload, headers=headers)
+        response = requests.request("POST", url + "/cfdi33/stamp/" + version + "/" + bs64, data=payload,
+                                    headers=headers)
         return response.text
 
+    @staticmethod
     def issue(self, url, token, xml, version, b64=False):
         if b64 is True:
             bs64 = "/b64"
@@ -31,7 +35,7 @@ class SWSDKPython:
             bs64 = ""
         boundary = "----=_Part_11_11939969.1490230712432"
         payload = "--" + boundary + "\r\nContent-Type: text/xml\r\nContent-Transfer-Encoding: binary\r\nContent-Disposition: " \
-                  "form-data; name=\"xml\"; filename=\"xml\"\r\n\r\n" + str(
+                                    "form-data; name=\"xml\"; filename=\"xml\"\r\n\r\n" + str(
             xml) + "\r\n--" + boundary + "-- "
         headers = {
             'Authorization': "bearer " + token,
@@ -41,10 +45,12 @@ class SWSDKPython:
                                     headers=headers)
         return response.text
 
-    def cancelByXML(self, url, token, xmlCancelacion):
+    @staticmethod
+    def cancel_by_xml(self, url, token, xmlCancelacion):
         boundary = "----=_Part_11_11939969.1490230712432"
         payload = "--" + boundary + "\r\nContent-Type: text/xml\r\nContent-Transfer-Encoding: binary\r\nContent-Disposition: " \
-                  "form-data; name=\"xml\"; filename=\"xml\"\r\n\r\n" + str(xmlCancelacion) + "\r\n--" + boundary + "-- "
+                                    "form-data; name=\"xml\"; filename=\"xml\"\r\n\r\n" + str(
+            xmlCancelacion) + "\r\n--" + boundary + "-- "
         headers = {
             'Authorization': "bearer " + token,
             'Content-Type': "multipart/form-data; boundary=\"" + boundary + "\""
@@ -52,9 +58,10 @@ class SWSDKPython:
         response = requests.request("POST", url + "/cfdi33/cancel/xml", data=payload, headers=headers)
         return response.text
 
-    def cancelByCSD(self, url, token, rfc, uuid, b64cert, b64key, password):
+    @staticmethod
+    def cancel_by_csd(self, url, token, rfc, uuid, b64cert, b64key, password):
         payload = "{ \"uuid\": \"" + uuid + "\",  \"password\": \"" + password + "\"," \
-                  "  \"rfc\": \"" + rfc + "\",    \"b64Cer\": \"" + b64cert + "\",  \"b64Key\": \"" + b64key + "\"}"
+                                                                                 "  \"rfc\": \"" + rfc + "\",    \"b64Cer\": \"" + b64cert + "\",  \"b64Key\": \"" + b64key + "\"}"
         headers = {
             'Authorization': "bearer " + token,
             'Content-Type': "application/json"
@@ -62,9 +69,10 @@ class SWSDKPython:
         response = requests.request("POST", url + "/cfdi33/cancel/csd", data=payload, headers=headers)
         return response.text
 
-    def cancelByPFX(self, url, token, rfc, uuid, b64Pfx, password):
+    @staticmethod
+    def cancel_by_pfx(self, url, token, rfc, uuid, b64Pfx, password):
         payload = "{ \"uuid\": \"" + uuid + "\",  \"password\": \"" + password + "\"," \
-                  "  \"rfc\": \"" + rfc + "\",    \"b64Pfx\": \"" + b64Pfx + "\" }"
+                                                                                 "  \"rfc\": \"" + rfc + "\",    \"b64Pfx\": \"" + b64Pfx + "\" }"
         headers = {
             'Authorization': "bearer " + token,
             'Content-Type': "application/json"
@@ -72,7 +80,8 @@ class SWSDKPython:
         response = requests.request("POST", url + "/cfdi33/cancel/pfx", data=payload, headers=headers)
         return response.text
 
-    def cancelByUUID(self, url, token, rfc, uuid):
+    @staticmethod
+    def cancel_by_uuid(self, url, token, rfc, uuid):
         headers = {
             'Authorization': "bearer " + token,
             'Content-Type': "application/json"
@@ -80,7 +89,8 @@ class SWSDKPython:
         response = requests.request("POST", url + "/cfdi33/cancel/" + rfc + "/" + uuid, headers=headers)
         return response.text
 
-    def accountBalance(self, url, token):
+    @staticmethod
+    def account_balance(self, url, token):
         headers = {
             'Authorization': "bearer " + token,
             'Content-Type': "application/json"
@@ -88,7 +98,8 @@ class SWSDKPython:
         response = requests.request("GET", url + "/account/balance", headers=headers)
         return response.text
 
-    def validateLRFC(self, url, token, lrfc):
+    @staticmethod
+    def validate_lrfc(self, url, token, lrfc):
         headers = {
             'Authorization': "bearer " + token,
             'Content-Type': "application/json"
@@ -96,7 +107,8 @@ class SWSDKPython:
         response = requests.request("GET", url + "/lrfc/" + lrfc, headers=headers)
         return response.text
 
-    def validateLCO(self, url, token, lco):
+    @staticmethod
+    def validate_lco(self, url, token, lco):
         headers = {
             'Authorization': "bearer " + token,
             'Content-Type': "application/json"
@@ -104,3 +116,14 @@ class SWSDKPython:
         response = requests.request("GET", url + "/lco/" + lco, headers=headers)
         return response.text
 
+
+def generate_token(self, uri, user, password):
+    self.uri = uri
+    self.user = user
+    self.password = password
+
+    if self.user is None or self.password is None:
+        raise Exception('Hace falta el usuario y/o contraseña')
+    headers = {'user': self.user, 'password': self.password, 'Cache-Control': "no-cache"}
+    response = requests.request("POST", self.uri + "/security/authenticate", headers=headers)
+    return response
--- UT_Servicios.py
+++ UT_Servicios.py
@@ -1,25 +1,44 @@
-import SWSDKPython
+from array import array
 import json
 import base64
 from idlelib.iomenu import encoding
+import unittest
+from SWSDKPython import SWSDKPython
 
-# Prueba
-url = "http://services.test.sw.com.mx"
-user = "demo"
-passwrd = "123456789"
+class Samples(unittest.TestCase):
 
-with open("basico.xml", "rb") as xml:
-    encoded_string = base64.b64encode(xml.read())
+    url = "http://services.test.sw.com.mx"
+    user = "demo"
+    passwrd = "123456789"
+
+    def test_getToken(self):
+        swservice: SWSDKPython = SWSDKPython()
+        resultToken = swservice.authenticate(self.url, self.user, self.passwrd)
+        token = json.loads(resultToken)
+        self.assertEqual(token['status'], 'success')
+
+    def test_stamp_v4_ok(self):
+        with open("basico.xml", "rb") as xml:
+            encoded_string = base64.b64encode(xml.read())
+        swservice: SWSDKPython = SWSDKPython()
+        resultToken = swservice.authenticate(self.url, self.user, self.passwrd)
+        token = json.loads(resultToken)
+        stamp = swservice.stamp(self.url, token['data']['token'], str(encoded_string, encoding), "v4", True)
+        result = json.loads(stamp)
+        self.assertEqual(result['message'], '307. El comprobante contiene un timbre previo.')
+
+    def test_account_balance(self):
+        swservice: SWSDKPython = SWSDKPython()
+        resultToken = swservice.authenticate(self.url, self.user, self.passwrd)
+        token = json.loads(resultToken)
+        accBalance = swservice.accountBalance(self.url, token['data']['token'])
+        acBaIdCliente = json.loads(accBalance)
+        self.assertEqual(acBaIdCliente['data']['idSaldoCliente'], '126eac70-425d-4493-87af-93505bfca746')
+
+    def test_valida_lrfc(self):
+        
 
 
-swservice = SWSDKPython.SWSDKPython()
-token = json.loads(swservice.authenticate(url, user, passwrd))
-print(token['data']['token'])
-rToken = token['data']['token']
 
-timbrar = swservice.stamp(url, rToken, str(encoded_string, encoding), "v4", True)
-print(timbrar)
-accBalance = swservice.accountBalance(url, token['data']['token'])
-
-print("Estado de cuenta "+accBalance)
-
+if __name__ == '__main__':
+    unittest.main()
