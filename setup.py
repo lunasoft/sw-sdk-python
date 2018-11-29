8585117b9b4eca9b676e6ac0cbf603b7ec68732a
Juan Gamez <juan.gamez@sw.com.mx>
2018-11-29 14:15:30 -0600

    Datos necesarios para la publicación del paquete.

--- setup.py
+++ setup.py
@@ -1,8 +1,22 @@
-from distutils.core import setup
-
-setup(
+import setuptools
+# read the contents of README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='latin_1') as f:
+    long_description = f.read()
+setuptools.setup(
     name='sw-sdk-python',
-    version='0.0.0.3dev',
-    packages=['AcceptReject','Auth','Balance','Cancelation','Issue','Pdf','Pendings','Relations','Stamp','StatusCfdi','Validate'],
-    long_description=open('README.md').read(),
+    version='0.0.0.3dev2',
+    author="Juan Gamez",
+    author_email="juan.gamez@sw.com.mx",
+    description="SDK para Timbrado en SmarterWeb",
+    url="https://github.com/lunasoft/sw-sdk-python",
+    packages=setuptools.find_packages(),
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
 )
