45633e4922bc9bb0f6b3566fc99818cd4efebd38
Juan Gamez <juan.gamez@sw.com.mx>
2018-11-29 14:24:13 -0600

    Encoding fix

--- setup.py
+++ setup.py
@@ -1,19 +1,14 @@
 import setuptools
-# read the contents of README file
-from os import path
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='latin_1') as f:
-    long_description = f.read()
 setuptools.setup(
     name='sw-sdk-python',
-    version='0.0.0.3dev2',
+    version='0.0.0.3dev5',
     author="Juan Gamez",
     author_email="juan.gamez@sw.com.mx",
     description="SDK para Timbrado en SmarterWeb",
     url="https://github.com/lunasoft/sw-sdk-python",
     packages=setuptools.find_packages(),
     long_description_content_type="text/markdown",
-    long_description=long_description,
+    long_description=open('README.md',encoding='utf-8').read(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
