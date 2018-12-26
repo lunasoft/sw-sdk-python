fbc55a9d15ca50e32bede7b537d1382ca3d6cede
Juan Gamez <juan.gamez@sw.com.mx>
2018-12-26 15:37:00 -0600

    Fix to concat bool

--- Csd/csd_request.py
+++ Csd/csd_request.py
@@ -6,7 +6,7 @@ from Csd.csd_response import CsdResponse
 class CsdRequest:
     @staticmethod
     def upload_csd(url, token, is_active, certificate_type, b64_cert, b64_key, password):
-        payload = "{ \"is_active\": \"" + is_active + "\",  \"password\": \"" + password + "\", \"certificate_type\": \"" + certificate_type + "\",    \"b64Cer\": \"" + b64_cert + "\",  \"b64Key\": \"" + b64_key + "\"}"
+        payload = "{ \"is_active\": \"" + str(is_active) + "\",  \"password\": \"" + password + "\", \"certificate_type\": \"" + certificate_type + "\",    \"b64Cer\": \"" + b64_cert + "\",  \"b64Key\": \"" + b64_key + "\"}"
         headers = {
             'Authorization': "bearer " + token,
             'Content-Type': "application/json"
