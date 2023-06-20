adc0e0a6562acc5a59869587f2a327b3c97301a7
NanoCarrillo <fernando.carrillo@sw.com.mx>
2023-06-20 16:22:30 -0600

    Se elimina variable no utilizada.

--- Pdf/PdfRequest.py
+++ Pdf/PdfRequest.py
@@ -1,14 +1,10 @@
 import requests
-import string
-import random
 import json
 from Pdf.PdfResponse import PdfResponse
 
 class PdfRequest():
     @staticmethod
     def generate_pdf(urlApi, token, xml, b64Logo , template_id, extras):
-        lst = [random.choice(string.ascii_letters + string.digits) for n in range(30)]
-        boundary = "".join(lst)
         headers = {
             'Authorization': "Bearer " + token,
             'Content-Type': "application/json"
