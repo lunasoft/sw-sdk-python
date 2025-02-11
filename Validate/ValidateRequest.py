969a9f6700eea806bdfd55731286028c1c362d30
Fernando Carrillo <108951984+FerCarrilloM@users.noreply.github.com>
2025-02-11 10:41:34 -0600

    Se ajusta meétodo de validar

--- Validate/ValidateRequest.py
+++ Validate/ValidateRequest.py
@@ -3,10 +3,12 @@ from Utils.requestHelper import RequestHelper
 
 class ValidateRequest:
     @staticmethod
-    def validate_xml(url, token, xml, base64=False):
-        bs64 = ""
-        if base64:
-            bs64 = "/b64"
-        endpoint = url + "/validate/cfdi/" + bs64
+    def validate_xml(url, token, xml, status=None):
+        path = ''
+        if status == True or status==None:
+            path = "/cfdi"
+        elif status == False:
+            path = "/cfdi?validatestatus=false"
+        endpoint = url + "/validate" + path
         response = RequestHelper.post_multipart_request(endpoint, token, xml)
         return ValidateResponse(response)
